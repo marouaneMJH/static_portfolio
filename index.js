6a6cd5fb7ae88eb8ea746d99d7fbe79b0f1d3210
marwan el moujahid <marwanmojahid007@gmail.com>
2024-02-21 20:14:14 +0100

    making the header and mouse trailer

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("#");
+// trailer animation
+
+const trailer=document.getElementById("trailer");
+const trailerPoint=document.getElementById("trailer-point");
+
+
+window,addEventListener("mousemove", el =>{
+    const   x=el.clientX - trailer.offsetWidth/2,
+            y=el.clientY -trailer.offsetWidth/2  ;
+    trailer.style.transform =  `translate(${x}px,${y}px)`;
+    trailer.style.transition = " 0.06s ease-in-out";
+    trailer.style.opacity =" 1";
+    trailerPoint.style.transform =  `translate(${x+14}px,${y+14}px)`;
+    trailerPoint.style.opacity =" 1";
+    // trailerPoint.style.transition = " 0.03s ease-in-out";
+
+
+});
+
+// trailer hover effect 
+
+
